932794f96d57e1d0518d076f56d7ccea09440589
Kauan Felipe <assis.pereira@escola.pr.gov.br>
2025-10-24 08:49:12 -0300

    Update #variaveis password, user.py

--- #variaveis password, user.py
+++ #variaveis password, user.py
@@ -1,4 +1,4 @@
-print("** Verficador de senha **")
+print("** Verificador de senha **")
 
 def create_data():
     user = input("Digite seu usuário: ")
@@ -8,14 +8,46 @@ def create_data():
 
     password = input("Confirme sua senha: ")
 
-if new_password != password:
-    print("Acesso negado!\n")
-    while new_password != password:
-        password = input("Digite novamente sua senha: ")
+    if new_password != password:
+        print("Acesso negado! Senha incorreta!\n")
+        while new_password != password:
+            password = input("Digite novamente sua senha: ")
     
     print("Acesso autorizado!")
 
-def aplication():
+def application():
     create_data()
 
-aplication()
+application()
+
+def verify_user():
+    user = "admin"
+    passw = "abc"
+
+    usuario == input("Digite o user cadastrado: ")
+    senha == input("Digite a user cadastrada: ")
+    if usuario != user:
+        print("User incorreto!")
+        while usuario != user:
+            usuario == input("Digite o user cadastrado: ")
+        if senha != passw:
+            print("Senha incorreta!")
+            while senha != passw:
+                senha == input("Digite o user cadastrado: ")
+    print("User logado com sucesso!")
+
+    def verify_login():
+        print("\n** LOGIN DE SISTEMA **")
+            opcao_login = input("Digite 1 para realizar o LOGIN e 2 para se CADASTRAR")
+            if opcao_login == 1 or opcao_login == 2:
+                if opcao_login == 1:
+                    verify_user()
+                else:
+                    create_data()
+                else:
+                    print("Opção não encontrada! ")
+
+def application():
+    verify_login()
+    
+application()
